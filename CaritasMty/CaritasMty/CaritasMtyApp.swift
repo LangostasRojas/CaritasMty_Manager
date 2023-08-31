34d43fa3d5bf810cf09c4e859d527914b6ee9169
Alumno <alumno@Mac-21.local>
2023-08-31 17:22:39 -0600

    Creado el proyecto de CaritasMTY en Xcode y agregado al repositorio

---
+++ CaritasMty/CaritasMty/CaritasMtyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaritasMtyApp.swift
+//  CaritasMty
+//
+//  Created by Alumno on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CaritasMtyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
