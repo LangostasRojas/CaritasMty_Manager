7c6de5d5069e464bdbfcd5538b6d16660e62bf88
Jair <142059157+KrakenDominguezz@users.noreply.github.com>
2023-09-27 15:52:27 -0600

    Manager Lista Api Funcional

---
+++ CaritasMty/CaritasMty/ListaIndividual.swift
@@ -0,0 +1,56 @@
+//
+//  TicketRow.swift
+//  CaritasMtyRepartidor
+//
+//  Created by Alumno on 06/09/23.
+//
+
+import SwiftUI
+
+struct ListaIndividual: View {
+    var colorDeTicket : String = "NoCompletado"
+    var ticket : Ticket
+    var body: some View {
+        ZStack{
+            Color("BgColor").ignoresSafeArea()
+            HStack{
+                
+                Image("Mapa").resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100)
+                    .padding(.trailing)
+                
+                VStack(alignment: .leading){
+                    Text("\(ticket.nombre)")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.bottom,5)
+                        .bold()
+                    Text("$ \(String(format: "%.2f", ticket.importe))")
+                        .font(.title)
+                        .foregroundColor(.white)
+                    
+                }
+                Spacer()
+                Circle()
+                    .frame(width: 20.0)
+                    .foregroundColor(Color(colorDeTicket))
+                    
+                    
+                
+                
+                
+                
+            }.padding()
+                .background(Color("BgColor"))
+            
+        }
+    }
+    
+}
+
+struct JugadorRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ListaIndividual(ticket: listaTickets[0])
+    }
+}
--- CaritasMty/CaritasMty/VistasDetailed.swift
+++ CaritasMty/CaritasMty/VistasDetailed.swift
@@ -1,4 +1,11 @@
 //
+//  VistaTicket.swift
+//  CaritasMtyRepartidor
+//
+//  Created by Alumno on 06/09/23.
+//
+
+//
 //  VistasDetailed.swift
 //  CaritasMty
 //
@@ -7,22 +14,32 @@
 
 import SwiftUI
 
-struct VistasDetailed: View {
+struct VistaTicket: View {
+    var ticket : Ticket
+
     var body: some View {
+        
         ZStack{
-            
+        
             Color("BgColor")
                 .ignoresSafeArea()
             VStack{
-                Text("Ticket - 201342")
-                    .foregroundColor(.white)
-                    .font(.system(size: 40))
-                    .fontWeight(.bold)
+                HStack{
+                    Text("Ticket - \(ticket.id)")
+                        .foregroundColor(.white)
+                        .font(.system(size: 34))
+                        .fontWeight(.bold)
+                        .padding(.leading, 40.0)
+                    Spacer()
+                }
+               
+                    
                 Rectangle()
                     .fill(Color("ColorAzulVerdePaleta"))
                     .frame(width: 170,height: 7)
                     .cornerRadius(20)
-                    .offset(x:-60,y:-25)
+                    .offset(x:-64,y:-25)
+                
                 Image("Mapa")
                     .resizable(resizingMode: .stretch)
                     .aspectRatio(contentMode: .fit)
@@ -31,9 +48,9 @@ struct VistasDetailed: View {
                     VStack{
                         Group{
                             Text("Nombre: ")
-                                .padding(10)
+                                .padding(.bottom,5)
                                 .fontWeight(.bold)
-                            Text("Kraken Alejandro Dominguez Ramirez")
+                            Text(ticket.nombre)
                                 .padding(.bottom,5.0)
                             Text("Dirección")
                                 .padding(.bottom,5.0)
@@ -42,9 +59,9 @@ struct VistasDetailed: View {
                             Text("Cantidad")
                                 .padding(.vertical,5)
                                 .fontWeight(.bold)
-                            Text("$ 2,000")
-                                .padding(.bottom,10)
-                        }
+                            Text("$ \(String(format: "%.2f", ticket.importe))").bold()
+                                .font(.system(size: 24))
+                                .padding(10).overlay(RoundedRectangle(cornerRadius: 15).stroke(Color("ColorAzulVerdePaleta"), lineWidth: 3))                        }
                         .font(.system(size: 20))
                         .padding(.horizontal,20.00)
                        
@@ -52,10 +69,32 @@ struct VistasDetailed: View {
                     .foregroundColor(.white)
 
                 Button(action: {
+                            
+                    if let repartidor = repartidor {
+                        completeTicket(ticketID: ticket.id, token: repartidor.accessToken) { success in
+                            
+                            if(success){
+                                print("EXITOOO!")
+                                
+                                if let listat = listaTicketsR{
+                                    
+                                    listaTicketsR?.remove(at: removeItem(arreglo: listat, ticket: ticket))
+                                    
+                                }
+                                
+                            }else{
+                                
+                                print("SUPER F no jalo")
+                                
+                            }
+                            
+                            
+                        }
+                    }
                     
                 }, label: {
-                    Text("Completar Entrega")
-                        .font(.title2)
+                    Text("Completar")
+                        .font(.title)
                         .bold()
                         .foregroundColor(.white)
                     
@@ -64,6 +103,7 @@ struct VistasDetailed: View {
                         .cornerRadius(30)
                         .shadow(color:.black,radius: 2,y:2)
                 })
+                .padding(.top, 10)
 
             
                 
@@ -75,8 +115,23 @@ struct VistasDetailed: View {
     }
 }
 
-struct VistasDetailed_Previews: PreviewProvider {
-    static var previews: some View {
-        VistasDetailed()
+func removeItem(arreglo: Array<Ticket>, ticket: Ticket) -> Int{
+    var i: Int = 0
+    while true{
+        if arreglo[i].id == ticket.id{
+            return i
+        }else{
+            i+=1
+        }
     }
 }
+
+
+struct VistaTicket_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        VistaTicket(ticket: listaTickets[0])
+    }
+}
+
+
