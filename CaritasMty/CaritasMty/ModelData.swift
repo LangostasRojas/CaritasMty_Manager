843999404c3f0041201d9d9ad69c986e8d088a85
Jair <142059157+KrakenDominguezz@users.noreply.github.com>
2023-09-27 17:17:47 -0600

    Cambiado Lista a Lista Manager

--- CaritasMty/CaritasMty/ModelData.swift
+++ CaritasMty/CaritasMty/ModelData.swift
@@ -17,3 +17,25 @@ func cargarDatos() -> Array<Ticket>{
     ]
     return tickets
 }
+
+func cargarDatosManager() -> Array<Array<Ticket>>{
+    var ticktsManager = [
+    [
+        Ticket(id: 4, nombre: "Minja", importe: 40400, direccion:"Ciudad Gotica", estatusVisita: 0),
+        Ticket(id: 5, nombre: "Juancho", importe: 100, direccion:"Ciudad Gotica", estatusVisita: 0),
+        Ticket(id: 8, nombre: "Jose", importe: 200000.2,direccion:"Ciudad Gotica", estatusVisita: 0)
+    ],
+    [
+        Ticket(id: 4, nombre: "Minja", importe: 40400, direccion:"Ciudad Gotica", estatusVisita: 0),
+        Ticket(id: 5, nombre: "Juancho", importe: 100, direccion:"Ciudad Gotica", estatusVisita: 0),
+        Ticket(id: 8, nombre: "Jose", importe: 200000.2,direccion:"Ciudad Gotica", estatusVisita: 0)
+    ],
+    [
+        Ticket(id: 4, nombre: "Minja", importe: 40400, direccion:"Ciudad Gotica", estatusVisita: 0),
+        Ticket(id: 5, nombre: "Juancho", importe: 100, direccion:"Ciudad Gotica", estatusVisita: 0),
+        Ticket(id: 8, nombre: "Jose", importe: 200000.2,direccion:"Ciudad Gotica", estatusVisita: 0)
+    ]
+    ]
+    return ticktsManager
+}
+
--- CaritasMty/GlobalVariables.swift
+++ CaritasMty/GlobalVariables.swift
@@ -9,3 +9,4 @@ import Foundation
 
 var repartidor: User?
 var listaTicketsR: Array<Ticket>?
+var listaTicketsManagers: Array<Array<Ticket>>?
