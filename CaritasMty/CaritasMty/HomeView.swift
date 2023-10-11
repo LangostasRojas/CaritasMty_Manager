319ad1313c182f77f836fb3346e285f5b34c082e
dani-suko <a01284684@tec.mx>
2023-10-11 15:31:21 -0600

    Tabview (incompleta)

--- CaritasMty/CaritasMty/HomeView.swift
+++ CaritasMty/CaritasMty/HomeView.swift
@@ -10,97 +10,16 @@ import SwiftUI
 struct HomeView: View {
 //    @State var usuario: Array<User> = []
     var body: some View {
-        NavigationStack{
-            VStack{
-                ZStack{
-                    
-                    Color("BgColor")
-                        .ignoresSafeArea()
-                    
-                    VStack{
-                        Circle()
-                            .fill(.gray)
-                            .frame(width: 200)
-                            .padding(.bottom,20)
-                        Text("Bienvenid@")
-                            .font(.largeTitle)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                        Rectangle()
-                            .fill(Color("ColorAzulVerdePaleta"))
-                            .frame(width: 150,height: 5)
-                            .cornerRadius(20)
-                        
-                        if let usuario = repartidor {
-                            
-                            Text("\(usuario.name)")
-                                .font(.system(size:37))
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .multilineTextAlignment(.center)
-                                .frame(width: 200)
-                                .padding(.bottom,30)
-                            
-                        }
-                        
-                        //                        NavigationLink("Tickets",destination: VistaListaConjunto())
-                        //                            .frame(width: 150)
-                        //                            .font(.title2)
-                        //                            .bold()
-                        //                            .foregroundColor(.white)
-                        //                            .padding(.vertical,15)
-                        //                            .padding(.horizontal,70)
-                        //                            .background(Color("ColorAzulVerdePaleta"))
-                        //                            .cornerRadius(50)
-                        //                            .shadow(color:.black,radius: 2,y:2)
-                        //                            .padding(.bottom,30)
-                        Button(action: {
-                            
-                        }, label: {
-                            Text("Tickets")
-                                .frame(width: 150)
-                                .font(.title2)
-                                .bold()
-                                .foregroundColor(.white)
-                                .padding(.vertical,15)
-                                .padding(.horizontal,70)
-                                .background(Color("ColorAzulVerdePaleta"))
-                                .cornerRadius(50)
-                                .shadow(color:.black,radius: 2,y:2)
-                        })
-                        .padding(.bottom,30)
-                        NavigationLink("Recolectores",destination: VistaListaConjunto())
-                            .frame(width: 150)
-                            .font(.title2)
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding(.vertical,15)
-                            .padding(.horizontal,70)
-                            .background(Color("ColorAzulVerdePaleta"))
-                            .cornerRadius(50)
-                            .shadow(color:.black,radius: 2,y:2)
-                            .padding(.bottom,30)
-                        
-                        
-                    }
-                    
-                    
-                    
-                    
+        TabView{
+            Home()
+                .tabItem {
+                    Label("Inicial", systemImage: "figure.basketball")
                 }
-                
-            }
-            
+            VistaListaConjunto()
+                .tabItem {
+                    Label("Cool 😈", systemImage: "basketball.circle")
+                }
         }
-        
-//        .onAppear(){
-//
-//            if let repartidor = repartidor {
-//                usuario = repartidor
-//            }
-//
-//        }
-        
     }
 }
 
